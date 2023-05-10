af9b73e40407082c7a4ef6076b224bf99ecc31c7
ArraisRonaldo <128387746+ArraisRonaldo@users.noreply.github.com>
2023-05-10 11:13:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 """This program counts the number of vowels in a string."""
-
 def count_vowels(string:str) -> int:
-    """
-    Takes in a string and returns the number of vowels in the string.
+    contador = 0
+    contador += string.lower().count("a")
+    contador += string.lower().count("e")
+    contador += string.lower().count("i")
+    contador += string.lower().count("o")
+    contador += string.lower().count("u")
 
-    Args:
-        string (str): The string to count vowels in.
-
-    Returns:
-        int: The number of vowels in the string.
-    """
+    return contador
